1db97fdf3a59da5c40ad072246b9e954dfc5132c
PÃÂ¡l SZÃÂK <szak.pal@gmail.com>
2018-01-21 11:21:33 +0100

    Integrate 500px - part 1
      - add minimalistic configuration handling
        to avoid hard coding sensitive information
      - add sample code about 500px api
      - save a response from 500px for testing purpose

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Homework
+
+## Configuration handling
+
+Your have to provide a valid configuration file under `./configuration/config.json`. If you need more information about available configuration options, please check `./configuration/config.sample.json`!
--- app/index.js
+++ app/index.js
@@ -1,7 +1,40 @@
 const express = require('express')
+const fs = require('fs-extra')
 const app = express()
+const API500px = require('500px')
+
+const CONFIG_FILE_LOCATION = './configuration/config.json'
 
 const foodDao = new (require('./dao/FoodDao.js'))()
+
+fs.readJson(CONFIG_FILE_LOCATION).then((config) => {
+  console.log(JSON.stringify(config, null, 2))
+
+  let api500px = new API500px(config['500px_consumer-key'])
+
+  let fetchImages = api500px.photos.searchByTag.bind(
+    api500px.photos,
+    'food',
+    {
+      'sort': 'created_at',
+      'rpp': '100',
+      'tags': true
+    })
+
+  fetchImages(function (error, results) {
+    if (error) {
+      console.log('something went wrong', error)
+      return
+    }
+    console.log('result', JSON.stringify(results, null, 2))
+  })
+}).catch((e) => {
+  console.error(`
+Error during configuration reading.
+
+Do you provide valid config.json?
+For further information pease check README.md`, e)
+})
 
 app.use('/api', require('./routes/api.js')(foodDao))
 app.use('/photos', require('./routes/photos.js')(foodDao))
